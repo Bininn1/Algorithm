f18142b9cb022145baddea11ad5583e04e040c39
Bininn1 <129733249+Bininn1@users.noreply.github.com>
2024-12-22 19:03:14 +0900

    [Bronze I] Title: 평균, Time: 232 ms, Memory: 21232 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int [] arr = new int[N];
+        int max = 0;
+        double result = 0;
+        for(int i = 0; i < N; i++) {
+            arr[i] = in.nextInt();
+        }
+        for(int i = 0; i < N; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        
+        for (int i = 0; i < N; i++) {
+            result += (double)arr[i]/max*100;
+        }
+       
+        
+        System.out.printf("%.2f",result/N);
+    }
+}
