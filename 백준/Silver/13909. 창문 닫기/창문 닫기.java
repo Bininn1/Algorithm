2aea42204661a6605e15fea6ddc5b1aff1f4dc4c
Bininn1 <129733249+Bininn1@users.noreply.github.com>
2025-01-15 00:30:26 +0900

    [Silver V] Title: 창문 닫기, Time: 172 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long N = in.nextLong();
+        System.out.println((int) Math.sqrt(N));
+    }
+}
